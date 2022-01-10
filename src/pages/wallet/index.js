cd156b58f43e1f1c57e5c5d058c6e7b5541a3c43
namhihi237 <yentth237@gmail.com>
2022-01-10 22:43:17 +0700

    UI wallet screen

--- src/pages/wallet/index.js
+++ src/pages/wallet/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { useNavigation } from '@react-navigation/native';
 import { useMutation } from '@apollo/client';
-
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 import { InputField, ButtonCustom, Toast, Loading, Header } from '../../components';
 import { SCREEN } from "../../constants"
 export default function Wallet(props) {
@@ -13,8 +13,51 @@ export default function Wallet(props) {
   return (
     <View style={styles.mainContainer}>
       <Header title={"NFood Shipper"} />
-      <View style={{ alignItems: 'center', marginTop: hp("5%"), marginBottom: 30 }}>
-        <Text fontSize="2xl" bold>Wallet</Text>
+      <View >
+        <View style={styles.incomeContainer}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <FontAwesome5 name="chart-line" size={hp("3.2%")} color="green" />
+            <Text ml="4" fontSize="lg">Thống kê thu nhập của bạn</Text>
+          </View>
+          <FontAwesome5 name="angle-right" size={hp("3.2%")} color="#444251" />
+        </View>
+
+        <View style={styles.reportContainer}>
+          <View style={styles.lineReport}>
+            <Text fontSize="md">Tiền giao hàng</Text>
+            <Text color="#F24F04" fontSize="md">0 đ</Text>
+          </View>
+          <View style={styles.line}></View>
+          <View style={styles.lineReport}>
+            <Text fontSize="md">Tổng mua hàng</Text>
+            <Text color="#F24F04" fontSize="md">0 đ</Text>
+          </View>
+          <View style={styles.line}></View>
+          <View style={styles.lineReport}>
+            <Text fontSize="md">Tiền thưởng</Text>
+            <Text color="#F24F04" fontSize="md">0 đ</Text>
+          </View>
+          <View style={styles.line}></View>
+          <View style={styles.lineReport}>
+            <Text fontSize="md">Số dư trong ví</Text>
+            <Text color="#F24F04" fontSize="md">0 đ</Text>
+          </View>
+          <View style={styles.line}></View>
+          <View style={styles.lineReport}>
+            <Text fontSize="md" bold>Tổng cộng</Text>
+            <Text color="#F24F04" fontSize="md" bold>0 đ</Text>
+          </View>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <FontAwesome5 name="wallet" size={hp("3.2%")} color="#444251" />
+          <Text ml="4" color="#fff" bold fontSize="md">Rút tiền khỏi ví</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button}>
+          <FontAwesome5 name="wallet" size={hp("3.2%")} color="#444251" />
+          <Text ml="4" color="#fff" bold fontSize="md">Nạp tiền vào ví</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -22,10 +65,49 @@ export default function Wallet(props) {
 
 const styles = StyleSheet.create({
   mainContainer: {
-    backgroundColor: '#fff',
     flex: 1,
     display: 'flex',
-    paddingBottom: hp("5%")
   },
+  incomeContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: wp("3%"),
+    paddingVertical: hp("2.5%"),
+    borderRadius: 10,
+    marginHorizontal: wp('4%'),
+    backgroundColor: '#fff',
+    marginTop: hp('2'),
+  },
+  reportContainer: {
+    backgroundColor: '#fff',
+    paddingHorizontal: wp("3%"),
+    paddingVertical: hp("2.5%"),
+    borderRadius: 10,
+    marginHorizontal: wp('4%'),
+    marginTop: hp('3.5%'),
+  },
+  lineReport: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  line: {
+    height: 1,
+    backgroundColor: '#E5E5E5',
+    marginTop: hp('2%'),
+    marginBottom: hp('2%'),
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    marginHorizontal: wp('10%'),
+    paddingHorizontal: wp("7%"),
+    borderRadius: 10,
+    paddingVertical: hp("1%"),
+    marginTop: hp('3%'),
+  }
 
 });
